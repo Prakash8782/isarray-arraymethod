7df0ce480b460fd7928a5f48cba2712b465fd769
Prakash8782 <prakash8782@gmail.com>
2022-09-19 15:11:26 +0530

    isarray

---
+++ isarray/main.js
@@ -0,0 +1,7 @@
+let colors=["Red","Blue","Green","Yellow","Pink"];
+console.log("colors : " ,colors);
+console.log("length of an array : "+colors.length);
+console.log("method : array.isArray()");
+let a=Array.isArray(colors);
+console.log("change in array : ", a);
+console.log("return : length of array after isArray(); is "+colors.length);
